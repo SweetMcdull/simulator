70451238cebb9b744acff97f8d94578176683575
leipeng <leipeng@haocang.com>
2020-09-29 17:46:48 +0800

    修改cli为click实现

--- simulator/cli.py
+++ simulator/cli.py
@@ -1,56 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import argparse
-
 import uvicorn
-
-from simulator import __version__
-
-
-def runserver(args):
-    uvicorn.run(args.app, host=args.host, port=args.port, reload=args.reload)
+import click
 
 
-def main():
-    parser = argparse.ArgumentParser(description="This is script help")
-    sub_parser = parser.add_subparsers(title='subcommands')
-    parser.add_argument(
-        "--version",
-        "-V",
-        action="version",
-        version=f"simulator version: {__version__}",
-        help="show the version",
-    )
+@click.group()
+def execute_from_command_line():
+    pass
 
-    runserver_parser = sub_parser.add_parser('runserver')
-    runserver_parser.add_argument(
-        '--app',
-        '-A',
-        default='app:app',
-        type=str,
-        help='FastAPI app'
-    )
-    runserver_parser.add_argument(
-        '--host',
-        '-H',
-        default='127.0.0.1',
-        type=str,
-        help='Server host'
-    )
-    runserver_parser.add_argument(
-        '--port',
-        '-P',
-        default=5000,
-        type=int,
-        help='Server port'
-    )
-    runserver_parser.add_argument(
-        '--reload',
-        '-R',
-        default=True,
-        type=bool,
-        help='Server reload'
-    )
-    runserver_parser.set_defaults(func=runserver)
-    parse_args = parser.parse_args()
-    parse_args.func(parse_args)
+
+@click.command()
+@click.option('--app', '-A', type=str, help='the server')
+@click.option('--host', '-H', type=str, default='127.0.0.1', help='server`s host')
+@click.option('--port', '-P', type=int, default=5000, help='server` port')
+@click.option('--reload', '-R', type=bool, default=False, help='reload or not')
+def runserver(app, host, port, reload):
+    uvicorn.run(app, host=host, port=port, reload=reload)
+
+
+execute_from_command_line.add_command(runserver)
--- simulator/manage.py
+++ simulator/manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from simulator.cli import main
+from simulator.cli import execute_from_command_line
 
 if __name__ == '__main__':
-    main()
+    execute_from_command_line()
