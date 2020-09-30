0a7033e267dd498dd66af9d9f1d2b8c3fb7fb086
leipeng <leipeng@haocang.com>
2020-09-30 11:50:06 +0800

    添加celery

--- simulator/api/api_v1/endpoints/monitor/inspect.py
+++ simulator/api/api_v1/endpoints/monitor/inspect.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter
 
+from simulator.core.manager import manager
+from simulator.response import Success
+
 router = APIRouter()
 
 
 @router.post('/active_tasks', summary='运行中的任务')
 def active_tasks():
-    pass
+    result = manager.active_tasks()
+    return Success(result=result)
